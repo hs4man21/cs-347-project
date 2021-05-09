0fae17d3337c68f406df36f487983f7899dc137f
hs4man21 <hs4man21@stanford.edu>
2021-05-09 12:45:42 -0700

    Removed leftover (and no longer applicable) comments from getting started example

--- popup.js
+++ popup.js
@@ -1,8 +1,6 @@
-// Initialize button with user's preferred color
 let changeColor = document.getElementById("submit");
 let feedback = document.getElementById("feedback");
 
-// When the button is clicked, inject setPageBackgroundColor into current page
 changeColor.addEventListener("click", async () => {
   const Http = new XMLHttpRequest();
   const url=`https://street-smarts-demo.herokuapp.com/send-test/${feedback.value}`
